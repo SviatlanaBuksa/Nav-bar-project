8ec1a17a1d973290d7f2dda904aa90a9a140c0b3
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-09-12 11:06:54 +0200

    Fixed css bugs

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+/* "use strict";
 
+let barsBtn = document.getElementById("bars-btn");
+let dropDownMenu = document.getElementById("menu");
+
+function showMenu() {}
+
+barsBtn.addEventListener("click", function () {
+  dropDownMenu.classList.toggle("show");
+});
+
+console.log(barsBtn);
+console.log(dropDownMenu); */
